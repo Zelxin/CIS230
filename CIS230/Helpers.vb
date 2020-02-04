05c2ccd5e1edf8feb7f8ad23fb5f48b4a3aca2ae
Vincent Vassallo <211355@canadorecollege.ca>
2020-02-04 18:49:22 -0500

    Add project files.

---
+++ CIS230/Helpers.vb
@@ -0,0 +1,14 @@
+﻿Imports System.IO
+Imports System.Reflection
+''' <summary>
+''' Helpful functions for CIS230
+''' </summary>
+Public Class Helpers
+
+    Public Function GetAssemblyDirectory() As String
+        Dim codeBase = Assembly.GetExecutingAssembly().CodeBase
+        Dim uriCB = New UriBuilder(codeBase)
+        Dim pathCB = Uri.UnescapeDataString(uriCB.Path)
+        Return Path.GetDirectoryName(pathCB)
+    End Function
+End Class
